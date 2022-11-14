85ae2fb732427fd63d3e5ea64ce80507e74c4c90
jonathanFS14 <jona08p8@stud.kea.dk>
2022-11-14 10:43:23 +0100

    tilføjet showmood metode

--- src/main/java/org/example/Animal.java
+++ src/main/java/org/example/Animal.java
@@ -3,4 +3,5 @@ public abstract class Animal {
     String name;
     public abstract void makeSound();
     public abstract boolean eat(String foodType);
+    public abstract void showMood(boolean mood);
 }
--- src/main/java/org/example/cat.java
+++ src/main/java/org/example/cat.java
@@ -8,4 +8,12 @@ public class cat extends Animal {
     public boolean eat(String foodType) {
         return true;
     }
+    @Override
+    public void showMood(boolean mood) {
+        if (mood == true){
+            System.out.println("logrer");
+        }
+        else System.out.println("knurrer");
+
+    }
 }
